a94b92472393ac72327cf94392f5ebca3b7d2885
msoodb <masoud.bolhassani@gmail.com>
2021-03-04 21:28:56 +0200

    deploy clean up

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,14 @@ VERSION=0
 RELEASE=2
 
 
-mkdir -p archive/fdesc-$VERSION
-rm -rf archive/fdesc-$VERSION/*
-cp -r src archive/fdesc-$VERSION/
-cp Makefile archive/fdesc-$VERSION/
-cp README.md archive/fdesc-$VERSION/
+mkdir -p fdesc-$VERSION
+rm -rf fdesc-$VERSION/*
+cp -r src fdesc-$VERSION/
+cp Makefile fdesc-$VERSION/
+cp README.md fdesc-$VERSION/
 
-tar -czvf archive/v$VERSION/fdesc-$VERSION.$RELEASE.tar.gz archive/fdesc-$VERSION
+
+tar -czvf archive/v$VERSION/fdesc-$VERSION.$RELEASE.tar.gz fdesc-$VERSION
 cp archive/v$VERSION/fdesc-$VERSION.$RELEASE.tar.gz archive/v$VERSION/fdesc-$VERSION.tar.gz
 
 # """ Copy spec file """
@@ -26,3 +27,6 @@ cp archive/v$VERSION/fdesc-$VERSION.tar.gz ~/rpmbuild/SOURCES
 # """ Download Source """
 # cd ~/rpmbuild/SOURCES
 # wget https://raw.githubusercontent.com/msoodb/fdesc/master/archive/v$VERSION/fdesc-$VERSION.tar.gz
+
+# """ Clean UP"""
+rm -rf fdesc-$VERSION
